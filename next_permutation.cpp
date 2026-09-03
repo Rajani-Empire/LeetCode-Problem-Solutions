911bc964c867d947b9fe7c44646c5487c8d3472c
Rajani Empire <rajani.swe.sust@gmail.com>
2026-09-03 22:58:48 +0600

    day10

---
+++ next_permutation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int pivot = -1;
+
+        for(int i = n-2;i>=0;i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+
+            }
+        }
+
+        if(pivot != -1){
+            int j= n-1;
+            while(nums[j] <= nums[pivot]){
+                j--;
+            }
+            swap(nums[j],nums[pivot]);
+        }
+
+        reverse(nums.begin() + pivot + 1, nums.end());
+        
+    }
+};
